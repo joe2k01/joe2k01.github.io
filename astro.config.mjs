7c9ae49e5627801c40eff3b7e628a73f75df5992
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-10-08 21:00:28 +0100

    Custom domain
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://joe2k01.github.io",
+  site: "https://barillari.me",
   integrations: [react(), tailwind()],
 });
